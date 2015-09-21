1b8bc429a62e8529ae90e4a449662a4b252d0c7e
Alex Manusu <alex.manusu@gmail.com>
2015-09-21 12:26:12 -0700

    Partial removal of chat terminology. Commit before renaming outside of chats factory

--- www/js/services.js
+++ www/js/services.js
@@ -9,10 +9,10 @@ angular.module('hn.services', ["firebase"])
 .factory('Chats', function($firebaseObject, $firebaseArray) {
   var APIUrl = "https://hacker-news.firebaseio.com/v0",
       topstoriesIndex,
-      topStories = [],
-      storyId = 10253963;
+      topStories = []
 
-  var getRef = function(storyId) {
+
+  var getItem = function(storyId) {
       return new Firebase(APIUrl).child("item").child(storyId);
   };
 
@@ -29,8 +29,8 @@ angular.module('hn.services', ["firebase"])
     var ref = new Firebase(APIUrl).child('topstories');
     ref.on('value', function(update){
        update.val().forEach(function (storyID, index) {
-         var storyRef = getRef(storyID);
-            storyRef.on('value', function(storyVal){
+         var storyElement = getItem(storyID);
+            storyElement.on('value', function(storyVal){
               topStories[index] = storyVal.val();
               console.log(topStories[index]);
               //console.log(storyVal.val());
@@ -41,47 +41,17 @@ angular.module('hn.services', ["firebase"])
   }()
   console.log(topStories)
 
-
-
-  var chats = topStories
-  var oldchats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
   return {
     all: function() {
-      return chats;
+      return topStories;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(topStory) {
+      topStories.splice(topStories.indexOf(topStory), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].$id === parseInt($id)) {
-          return chats[i];
+    get: function(storyId) {
+      for (var i = 0; i < topStories.length; i++) {
+        if (topStories[i].$id === parseInt($id)) {
+          return topStories[i];
         }
       }
       return null;
